ec97641191619747f3460d052fbd184a7a2f19c0
Jakub Korsak <jakub.korsak@protonmail.com>
2020-11-29 23:54:52 +0100

    basic stuff and docker integration kinda

--- obturaculum.go
+++ obturaculum.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var client *redis.Client
+var store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
+var templates *template.Template
+
 func main() {
-	fmt.Println("vim-go")
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, "/", 302)
 }
